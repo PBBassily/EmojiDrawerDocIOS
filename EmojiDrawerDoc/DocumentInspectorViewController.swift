797e306433f631836ceaeb26b8fccdc36c4d7fdc
PBBassily <paulaboules@pharos-mac-sever.local>
2018-09-24 13:59:25 +0200

    adding new inspector view controller

---
+++ EmojiDrawerDoc/DocumentInspectorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DocumentInspectorViewController.swift
+//  EmojiDrawerDoc
+//
+//  Created by Paula Boules on 9/24/18.
+//  Copyright © 2018 Paula Boules. All rights reserved.
+//
+
+import UIKit
+
+class DocumentInspectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
